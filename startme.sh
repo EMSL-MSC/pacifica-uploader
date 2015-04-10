63575e3f2416cc2bf78637f44e786546eb4bc3ae
dmlb2000 <dmlb2000@gmail.com>
2015-04-10 09:27:34 -0700

    add the setup super user stuff too

--- startme.sh
+++ startme.sh
@@ -4,5 +4,4 @@ rabbitmq-server &
 sleep 5
 cd /app
 su daemon -s /bin/bash -c 'cd /app; celery -A UploadServer worker --loglevel=info' &
-su daemon -s /bin/bash -c 'cd /app; python manage.py migrate'
 su daemon -s /bin/bash -c 'cd /app; python manage.py runserver 0.0.0.0:8000'
