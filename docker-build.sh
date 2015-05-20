20c1bce18e3cb1b05c1185e428d28da84ec6be62
David Brown <david.brown@pnl.gov>
2015-05-19 20:02:53 -0700

    tag the thing right

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
 docker pull rabbitmq
+docker tag rabbitmq artifactory.pnnl.gov/myemslrabbit
 docker build -t localhost/myemslcontroller .
 docker build -t artifactory.pnnl.gov/myemslcelery - < Dockerfile.celery
 docker build -t artifactory.pnnl.gov/myemsldjango - < Dockerfile.django
