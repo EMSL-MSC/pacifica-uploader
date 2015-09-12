e624d2e9e78a6aa92d8369b71dc68c2a588072a3
derekhop <derek.hopkins@pnl.gov>
2015-09-11 17:10:00 -0700

    got rid of unused models

--- home/admin.py
+++ home/admin.py
@@ -3,9 +3,5 @@ register the Django models
 """
 
 from django.contrib import admin
-from home.models import Filepath
-from home.models import Metadata
 
 # Register your models here.
-admin.site.register(Filepath)
-admin.site.register(Metadata)
