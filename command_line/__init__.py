11c15fb8ad614db5f806b7ab473aea66d6caf958
derekhop <derek.hopkins@pnl.gov>
2015-12-02 12:53:30 -0800

    working demo

--- command_line/__init__.py
+++ command_line/__init__.py
@@ -14,6 +14,8 @@ from getpass import getpass
 import datetime
 
 from home import tasks
+from home import session_data
+from home import instrument_server
 
 def _add_file_cb( option, opt, value, parser ):
     """
@@ -26,8 +28,6 @@ def _add_file_cb( option, opt, value, parser ):
             The filename that was passed to this callback
         parser
             The OptionParser that calls the callback
-            
-    @note: The file_list is a list of tuples formatted as [ (file_path, file_arcname) ... ]
     """
 
     # Try to expand the value argument using wildcards.
@@ -40,30 +40,16 @@ def _add_file_cb( option, opt, value, parser ):
         parser.error( "File argument resolved to 0 existing files: %s" % value )
                 
     # Each entry in the file list is a tuple of the file's absolute path and the relative file name
-    file_tuples = []
+    files = []
     abspath = os.path.abspath( parser.values.work_dir )
     for file_name in file_glob:
         if file_name[:1] != '/':
             file_name = os.path.abspath( os.path.join( parser.values.work_dir, file_name ) )
-        altname = None
-        lap = len( abspath )
-        if file_name == abspath:
-            altname = ""
-        elif file_name[:lap+1] == "%s/" %(abspath):
-            altname = file_name[lap+1:]
-        file_tuples.append( ( file_name, altname ) )
-        logger.debug( "Abspath = %s" %( abspath ) )
-        logger.debug( "Adding file %s as %s" %( file_name, altname ) )
-                       
-    # If the glob has only one entry and an arcname argument was supplied, use it
-    if len( file_glob ) == 1 and parser.values.alt_name != None:
-        file_tuples[0] = ( file_tuples[0][0], parser.values.alt_name )
         
-    # No matter what happens, the alt name is cleared
-    parser.values.alt_name = None
+        files.append( file_name )
     
     # Add the new file names to the parser's file name list
-    parser.values.file_list.extend( file_tuples )
+    parser.values.file_list.extend( files )
 
 
 def add_usage( parser ):
@@ -89,7 +75,11 @@ def add_options( parser ):
 
     # Set the directory in which to work
     parser.add_option( '-c', '--cwd', type='string', action='store', dest='work_dir', default=os.getcwd(),
-                       help="Change the bundler's working directory to DIR", metavar='DIR' )
+                       help="Change the uploader's working directory to DIR", metavar='DIR' )
+
+    # Set the directory in which to bundle
+    parser.add_option( '-t', '--tar', type='string', action='store', dest='tar_dir', default=os.getcwd(),
+                       help="Set the uploader's tar directory to DIR", metavar='DIR' )
 
     # Set the instrument to use
     parser.add_option( '-i', '--instrument', type='string', action='store', dest='instrument', default='',
@@ -118,7 +108,7 @@ def check_options( parser, bundle_name_optional=True ):
     Performs custom option checks for this module given an OptionParser
     """
     current_time = datetime.datetime.now().strftime("%m.%d.%Y.%H.%M.%S")
-    parser.values.bundle_name = os.path.join(parser.values.work_dir, current_time + ".tar")
+    parser.values.bundle_name = os.path.join(parser.values.tar_dir, current_time + ".tar")
 
     parser.values.password = getpass( "Enter Password:" )
 
@@ -128,14 +118,36 @@ def upload_from_options( parser ):
     """
     Upload files based upon command line options supecified in an OptionParser
     """
+    # populate the session
+    session = session_data.SessionState()
 
+    filtered = session.files.filter_selected_list(parser.values.file_list)
+
+    common_path = os.path.commonprefix(filtered)
+
+    #get rid of dangling prefixes
+    common_path, tail = os.path.split(common_path)
+    common_path = os.path.join(common_path, '')
+
+    # used to arc names
+    session.files.common_path = common_path
+
+    # build archive path
+    configuration = instrument_server.InstrumentConfiguration()
+    configuration.instrument_short_name = parser.values.instrument
+    session.proposal_id = parser.values.proposal
+    session.config = configuration
+    session.get_archive_tree()
+
+    # get the file tuples (local name, archive name) to bundle
+    tuples = session.files.get_bundle_files(parser.values.file_list)
 
     tasks.upload_files( bundle_name=parser.values.bundle_name,
                         instrument_name=parser.values.instrument,
                         proposal=parser.values.proposal,
-                        file_list=parser.values.file_list,
-                        bundle_size=0,
-                        groups=parser.values.groups,
+                        file_list=tuples,
+                        bundle_size=session.files.bundle_size,
+                        groups=parser.groups,
                         server=parser.values.server,
                         user=parser.values.user,
                         password=parser.values.password)
@@ -151,7 +163,7 @@ def main():
         upload_from_options( parser )
 
     except Exception as err:
-        print >> sys.stderr, "CLU dieded: %s" % err.msg
+        print >> sys.stderr, "CLU dieded: %s" % err
 
 
 if __name__ == '__main__':
