2dbe68416710fe4407f3f79292473c40ee47795b
derekhop <derek.hopkins@pnl.gov>
2015-12-16 17:06:29 -0800

    version 99.6

--- home/views.py
+++ home/views.py
@@ -64,7 +64,7 @@ session = session_data.SessionState()
 configuration = instrument_server.InstrumentConfiguration()
 
 # development version
-version = '0.99.5'
+version = '0.99.6'
 
 def login_user_locally(request):
     """
