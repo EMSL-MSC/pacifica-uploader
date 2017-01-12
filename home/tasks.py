5d886606d7683919bde3c756164ef712ef45895e
derekhop <derek.hopkins@pnl.gov>
2017-01-11 17:01:22 -0800

    pylint, exception handling mods

--- home/tasks.py
+++ home/tasks.py
@@ -59,6 +59,11 @@ def ping():
 
 # tag to show this def as a celery task
 
+# pylint: disable=too-many-arguments
+# justification: perfect amount of arguments
+
+# pylint: disable=broad-exception
+# justification: we want to report and log any error at the highest level
 
 @shared_task
 def upload_files(ingest_server='',
@@ -71,69 +76,64 @@ def upload_files(ingest_server='',
     task created on a separate Celery process to bundle and upload in the background
     status and errors are pushed by celery to the main server through RabbitMQ
     """
+    try:
+        target_dir = os.path.dirname(bundle_name)
+        if not os.path.isdir(target_dir):
+            TaskComm.task_state('ERROR', 'Bundle directory does not exist')
+            return 'Upload Failed'
 
-    target_dir = os.path.dirname(bundle_name)
-    if not os.path.isdir(target_dir):
-        TaskComm.task_state('ERROR', 'Bundle directory does not exist')
-        return 'Upload Failed'
+        TaskComm.task_state("PROGRESS", "Cleaning previous uploads")
 
-    TaskComm.task_state("PROGRESS", "Cleaning previous uploads")
+        # clean tar directory
+        # if CLEAN_TAR:
+        #    err_str = clean_target_directory(target_dir)
+        #    if err_str:
+        #        TaskComm.task_state('PROGRESS', err_str)
 
-    # clean tar directory
-    # if CLEAN_TAR:
-    #    err_str = clean_target_directory(target_dir)
-    #    if err_str:
-    #        TaskComm.task_state('PROGRESS', err_str)
-
-    # initial state pushed through celery
-    TaskComm.task_state("PROGRESS", "Starting Bundle/Upload Process")
-
-    bundle(bundle_name=bundle_name,
-           file_list=file_list,
-           meta_list=meta_list,
-           bundle_size=bundle_size)
-
-    TaskComm.task_state("PROGRESS", "Completed Bundling")
-
-    if tartar:
-        # create the file tuple list of 1 file
-        fname = os.path.basename(bundle_name)
-
-        file_tuples = []
-        file_tuples.append((bundle_name, fname))
-
-        bundle_size = os.path.getsize(bundle_name)
-
-        # dual extension indicates tartar
-        bundle_name += '.tar'
+        # initial state pushed through celery
+        TaskComm.task_state("PROGRESS", "Starting Bundle/Upload Process")
 
         bundle(bundle_name=bundle_name,
-               file_list=file_tuples,
+               file_list=file_list,
                meta_list=meta_list,
                bundle_size=bundle_size)
 
-    TaskComm.task_state("PROGRESS", "Starting Upload")
+        TaskComm.task_state("PROGRESS", "Completed Bundling")
 
-    result = upload(bundle_name=bundle_name, ingest_server=ingest_server)
+        if tartar:
+            # create the file tuple list of 1 file
+            fname = os.path.basename(bundle_name)
 
-    if not result:
-        TaskComm.task_state('FAILURE', "Uploader dieded. We don't know why it did")
+            file_tuples = []
+            file_tuples.append((bundle_name, fname))
 
-    try:
+            bundle_size = os.path.getsize(bundle_name)
+
+            # dual extension indicates tartar
+            bundle_name += '.tar'
+
+            bundle(bundle_name=bundle_name,
+                   file_list=file_tuples,
+                   meta_list=meta_list,
+                   bundle_size=bundle_size)
+
+        TaskComm.task_state("PROGRESS", "Starting Upload")
+
+        result = upload(bundle_name=bundle_name, ingest_server=ingest_server)
+
+        if not result:
+            TaskComm.task_state('FAILURE', "Uploader dieded. We don't know why it did")
+
         status = json.loads(result)
-    except Exception, ex:
-        TaskComm.task_state('FAILURE', result)
-        return False
 
-    if status['state'] != 'OK':
-        TaskComm.task_state('FAILURE', result)
-        return False
+        if status['state'] != 'OK':
+            TaskComm.task_state('FAILURE', result)
+            return False
 
-    try:
         print "rename"
         tar_man.rename_tar_file(target_dir, bundle_name, status['job_id'])
         TaskComm.task_state('DONE', result)
         return True
     except Exception, ex:
-        TaskComm.task_state('FAILURE', ex.message +' Unable to rename ' + bundle_name)
+        task_error('FAILURE','tasks: upload_files :' + ex.message)
         return False
