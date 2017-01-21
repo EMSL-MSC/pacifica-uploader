93e40af644bf8f13359e7c1d9e31f89bc5e4fd75
David Brown <dmlb2000@gmail.com>
2017-01-20 20:42:43 -0800

    we don't need to do all the docker compose stuff with run lints

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -9,18 +9,17 @@ if [ "$RUN_LINTS" = "true" ]; then
   pip install pre-commit
 else
   pip install codeclimate-test-reporter coverage nose pytest
-fi
-
-docker-compose build baseimage
-docker-compose up -d
-MAX_TRIES=60
-HTTP_CODE=$(curl -sL -w "%{http_code}\\n" localhost:8121/keys -o /dev/null || true)
-while [[ $HTTP_CODE != 200 && $MAX_TRIES > 0 ]] ; do
-  sleep 1
+  docker-compose build baseimage
+  docker-compose up -d
+  MAX_TRIES=60
   HTTP_CODE=$(curl -sL -w "%{http_code}\\n" localhost:8121/keys -o /dev/null || true)
-  MAX_TRIES=$(( MAX_TRIES - 1 ))
-done
-docker run -it --rm --net=pacificauploader_default -e METADATA_URL=http://metadataserver:8121 -e PYTHONPATH=/usr/src/app pacifica/metadata python test_files/loadit.py
-docker-compose stop uploaddjango uploadcelery uploadamqp
-docker-compose stop policyserver
-docker-compose start policyserver
+  while [[ $HTTP_CODE != 200 && $MAX_TRIES > 0 ]] ; do
+    sleep 1
+    HTTP_CODE=$(curl -sL -w "%{http_code}\\n" localhost:8121/keys -o /dev/null || true)
+    MAX_TRIES=$(( MAX_TRIES - 1 ))
+  done
+  docker run -it --rm --net=pacificauploader_default -e METADATA_URL=http://metadataserver:8121 -e PYTHONPATH=/usr/src/app pacifica/metadata python test_files/loadit.py
+  docker-compose stop uploaddjango uploadcelery uploadamqp
+  docker-compose stop policyserver
+  docker-compose start policyserver
+fi
