4866c28e6211f1f8da07963f0157d1b5209e71b1
Ken Auberry <kenneth.auberry@pnnl.gov>
2017-12-14 02:35:09 -0800

    Adding in more comprehensive logging

--- uploader/__init__.py
+++ uploader/__init__.py
@@ -7,6 +7,14 @@ import os
 import sys
 import requests
 from home.task_comm import task_error, TaskComm
+import logging
+from django.conf import settings
+
+fmt = getattr(settings, 'LOG_FORMAT', None)
+lvl = getattr(settings, 'LOG_LEVEL', logging.DEBUG)
+
+logging.basicConfig(format=fmt, level=lvl)
+logging.debug("Logging started on %s for %s" % (logging.root.name, logging.getLevelName(lvl)))
 
 
 class Uploader(object):
